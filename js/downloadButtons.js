a212fd8eef04ba54ffc4234760ca643c555b4711
Tyler Scott <tybugman@hotmail.com>
2017-04-16 15:24:18 -0600

    update and bugfixes

--- js/downloadButtons.js
+++ js/downloadButtons.js
@@ -22,14 +22,14 @@ document.write('\
         <img class="myicon" src="images/overwatchIcon.png" alt="Overwatch"></a></li>\
       <li><a class="btn-floating transparent tooltipped" data-position="left" data-delay="50" data-tooltip="Diablo3" href="http://us.battle.net/d3/en/profile/BigTScott-1628/hero/53566837">\
         <img class="myicon" src="images/d3Icon.png" alt="D3"></a></li>\
-      <li><a class="btn-floating transparent tooltipped" data-position="left" data-delay="50" data-tooltip="Steam" href="http://steamcommunity.com/id/BigTScott">\
-        <img class="myicon" src="images/steamIcon.png" alt="Steam"></a></li>\
-      <li><a class="btn-floating transparent tooltipped" data-position="left" data-delay="50" data-tooltip="Facebook" href="https://www.facebook.com/BigT.Scott">\
-        <img class="myicon" src="images/fbIcon.png" alt="Facebook"></a></li>\
-      <li><a class="btn-floating transparent tooltipped" data-position="left" data-delay="50" data-tooltip="LinkedIn" href="https://www.linkedin.com/in/tylerscottcs">\
-        <img class="myicon" src="images/linkedinIcon.png" alt="LinkeIn"></a></li>\
-      <li><a class="btn-floating transparent tooltipped" data-position="left" data-delay="50" data-tooltip="GitHub" href="https://github.com/tscott8">\
-        <img class="myicon" src="images/githubIcon.png" alt="GitHub"></a></li>\
+      <li><a class="btn-floating black tooltipped" data-position="left" data-delay="50" data-tooltip="Steam" href="http://steamcommunity.com/id/BigTScott">\
+        <i class="left fa fa-steam fa-2x"></i></a></li>\
+      <li><a class="btn-floating blue darken-4 tooltipped" data-position="left" data-delay="50" data-tooltip="Facebook" href="https://www.facebook.com/BigT.Scott">\
+        <i class="center fa fa-facebook fa-2x"></i></a></li>\
+      <li><a class="btn-floating blue darken-3 tooltipped" data-position="left" data-delay="50" data-tooltip="LinkedIn" href="https://www.linkedin.com/in/tylerscottcs">\
+        <i class="center fa fa-linkedin fa-2x"></i></a></li>\
+      <li><a class="btn-floating purple darken-3 tooltipped" data-position="left" data-delay="50" data-tooltip="GitHub" href="https://github.com/tscott8">\
+        <i class="center fa fa-github fa-2x"></i></a></li>\
     </ul>\
   </div>\
 ');
