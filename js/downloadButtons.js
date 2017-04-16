f72921b55b1aaeff2b39d4e74f56cd68ca084e91
Tyler Scott <tybugman@hotmail.com>
2017-04-16 16:02:23 -0600

    update

--- js/downloadButtons.js
+++ js/downloadButtons.js
@@ -17,19 +17,19 @@ document.write('\
     <a href="Resume.pdf" class="btn-floating btn-large teal tooltipped" data-position="left" data-delay="50" data-tooltip="Download Resume">\
       <i class="large material-icons">file_download</i>\
     </a>\
-    <ul class="online">\
+    <ul class="hide-on-small-only	">\
       <li><a class="btn-floating transparent tooltipped" data-position="left" data-delay="50" data-tooltip="Overwatch" href="https://playoverwatch.com/en-us/career/pc/us/BigTScott-1628">\
         <img class="myicon" src="images/overwatchIcon.png" alt="Overwatch"></a></li>\
       <li><a class="btn-floating transparent tooltipped" data-position="left" data-delay="50" data-tooltip="Diablo3" href="http://us.battle.net/d3/en/profile/BigTScott-1628/hero/53566837">\
         <img class="myicon" src="images/d3Icon.png" alt="D3"></a></li>\
       <li><a class="btn-floating black tooltipped" data-position="left" data-delay="50" data-tooltip="Steam" href="http://steamcommunity.com/id/BigTScott">\
-        <i class="left fa fa-steam fa-2x"></i></a></li>\
+        <i class="large fa fa-steam fa-2x"></i></a></li>\
       <li><a class="btn-floating blue darken-4 tooltipped" data-position="left" data-delay="50" data-tooltip="Facebook" href="https://www.facebook.com/BigT.Scott">\
-        <i class="center fa fa-facebook fa-2x"></i></a></li>\
+        <i class="center large fa fa-facebook fa-2x"></i></a></li>\
       <li><a class="btn-floating blue darken-3 tooltipped" data-position="left" data-delay="50" data-tooltip="LinkedIn" href="https://www.linkedin.com/in/tylerscottcs">\
-        <i class="center fa fa-linkedin fa-2x"></i></a></li>\
+        <i class="center large fa fa-linkedin fa-2x"></i></a></li>\
       <li><a class="btn-floating purple darken-3 tooltipped" data-position="left" data-delay="50" data-tooltip="GitHub" href="https://github.com/tscott8">\
-        <i class="center fa fa-github fa-2x"></i></a></li>\
+        <i class="center large fa fa-github fa-2x"></i></a></li>\
     </ul>\
   </div>\
 ');
