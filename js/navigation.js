fce7ff57fbb600f8fadda6663b9abf4759d6b5e3
Tyler Scott <tybugman@hotmail.com>
2017-05-18 13:00:53 -0600

    made navigation js more dynamic

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,4 @@
-var array = [
+var portfolio_links = [
   {label:'Tableiness', url:'portfolio.html#tableiness'},
   {label:'SuperRecruiter', url:'portfolio.html#superRecruiter'},
   {label:'Walle & Eva', url:'portfolio.html#walleEva'},
@@ -7,31 +7,40 @@ var array = [
   {label:'Shift', url:'portfolio.html#Shift'},
   {label:'Arcane', url:'portfolio.html#Arcane'},
 ]
-var pEl = $.map(array, function(obj, i) {
+var p = $.map(portfolio_links, function(obj, i) {
   return '<li><a class="white-text" href="'+obj.url+'">'+obj.label+'</a></li>';
 });
 
+var nav_links = [
+  {label:'ABOUT ME', url:'index.html#about'},
+  {label:'SKILLS', url:'index.html#skills'},
+  {label:'PORTFOLIO', url:'portfolio.html'},
+  {label:'RESUME', url:'Resume.pdf'},
+  {label:'CONTACT', url:'index.html#contact'},
+]
+function get_nav_links (type) {
+  var n = $.map(nav_links, function(obj, i) {
+    if(obj.label === "PORTFOLIO")
+     return '<li><a class="white-text" href="'+obj.url+'" data-induration="300" data-outduration="225" data-beloworigin="true" data-hover="true" data-activates="'+type+'"">'+obj.label+'</a></li>';
+    else
+      return '<li><a class="white-text" href="'+obj.url+'">'+obj.label+'</a></li>';
+  });
+  console.log(n);
+  return n;
+}
 document.write('\
 <div class="navbar-fixed">\
-  <ul id="portfolioDrop" class="dropdown-content grey darken-4">'+pEl.join("")+'</ul>\
-  <ul id="portfolioDrop-mobile" class="dropdown-content dropdown-content-mobile grey darken-4">'+pEl.join("")+'</ul>\
+  <ul id="portfolioDrop" class="dropdown-content grey darken-4">'+p.join("")+'</ul>\
+  <ul id="portfolioDrop-mobile" class="dropdown-content dropdown-content-mobile grey darken-4">'+p.join("")+'</ul>\
   <nav class="grey darken-4" role="navigation">\
     <div class="nav-wrapper container">\
         <a href="#!" class="brand-logo"> <img class="mylogo" src="images/logo.png" /></a>\
         <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>\
         <ul class="right hide-on-med-and-down">\
-          <li><a class="white-text" href="index.html#about">ABOUT ME</a></li>\
-          <li><a class="white-text" href="index.html#skills">SKILLS</a></li>\
-          <li><a class="white-text dropdown-button" href="portfolio.html" data-induration="300" data-outduration="225" data-beloworigin="true" data-hover="true" data-activates="portfolioDrop">PORTFOLIO<i class="material-icons right white-text">arrow_drop_down</i></a></li>\
-          <li><a class="white-text" href="Resume.pdf">RESUME</a></li>\
-          <li><a class="white-text" href="index.html#contact">CONTACT</a></li>\
+          '+get_nav_links('portfolioDrop').join("")+'\
         </ul>\
         <ul class="side-nav grey darken-4" id="nav-mobile">\
-          <li><a class="white-text" href="index.html#about">ABOUT ME</a></li>\
-          <li><a class="white-text" href="index.html#skills">SKILLS</a></li>\
-          <li><a class="white-text dropdown-button" href="portfolio.html" data-induration="300" data-outduration="225" data-beloworigin="true" data-activates="portfolioDrop-mobile">PORTFOLIO<i class="material-icons right  white-text">arrow_drop_down</i></a></li>\
-          <li><a class="white-text" href="Resume.pdf">RESUME</a></li>\
-          <li><a class="white-text" href="index.html#contact">CONTACT</a></li>\
+          '+get_nav_links('portfolioDrop-mobile').join("")+'\
       </ul>\
     </div>\
   </nav>\
